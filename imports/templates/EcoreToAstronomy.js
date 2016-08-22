9edf94e0f062288b48ecb6f0d8e7047bc01ed0b6
nicolasferry <nicolas.ferry@sintef.no>
2016-08-22 13:35:32 +0200

    Fix some ordering issues

--- imports/templates/EcoreToAstronomy.js
+++ imports/templates/EcoreToAstronomy.js
@@ -1,7 +1,11 @@
-import { Ecore } from 'ecore/dist/ecore.xmi';
+import {Ecore} from 'ecore/dist/ecore.xmi';
+import {Class} from 'meteor/jagi:astronomy';
 
 var fs = Npm.require('fs');
 var DustJS = Npm.require("dustjs-helpers");
+
+// Keep line breaks
+//DustJS.optimizers.format = function (ctx, node) { return node; };
 
 function loadDustTemplate(name) {
     var template = fs.readFileSync(process.cwd().split('.meteor')[0] + "imports/templates/" + name + ".dust", "UTF8").toString(), compiledTemplate = DustJS.compile(template, name);
@@ -18,17 +22,22 @@ loadDustTemplate('Export');
 var fileContents = fs.readFileSync(process.cwd().split('.meteor')[0] + 'resources/BigML-metamodel.ecore', 'utf8');
 var resourceSet = Ecore.ResourceSet.create();
 var resource = resourceSet.create({uri: '/resources/BigML-metamodel.ecore'});
-var result= "";
+var result = "";
 
 try {
     //Parse the XMI: we have objects now
     resource.parse(fileContents, Ecore.XMI);
     //Get the root
     var ePackage = resource.get('contents').first();
-    //The model that will contain the data to fill the template
-    var model = {};
+    //The map that contains all the models with inherited
+    var mapInherited = new Map();
+    //The map that contains all the models without inheritance
+    var map = new Map();
     //We go through all the concept that compose the MM and compute model
     ePackage.get('eClassifiers').map(function (c) {
+
+        //The model that will contain the data to fill the template
+        var model = {};
         model.elementName = c.get('name');
 
         //We retrieve the information related to the attributes
@@ -39,48 +48,67 @@ try {
                 attribute.attributeName = f.get('name');
                 var eType = f.get('eType').get('name');
                 if (f.isTypeOf('EReference')) {
-                    attribute.type = "["+eType+"]";
+                    attribute.type = "[" + eType + "]";
                     attribute.default = "default() { return []; }"
                 } else {
-                    attribute.default = "default: ''"
                     if (eType == 'EString') {
-                        attribute.type = "String";
+                        attribute.type = "Boolean";
+                        attribute.default = "default: false";
                     } else {
                         attribute.type = "Boolean";
+                        attribute.default = "default: false";
                     }
                 }
                 model.attributes.push(attribute);
             });
-
-            //To select the type of template we are looking for
-            var typeTemplate = "";
-            if (c.get('eSuperTypes').size() > 0) {
-                c.get('eSuperTypes').map(function (s) {
-                    model.parentClass = s.get('name');
-                });
-                typeTemplate = "ClassInherited";
-            } else {
-                typeTemplate = "ClassWithoutInheritance";
-            }
-
-            //We fill the template
-            DustJS.render(typeTemplate, model, function (err, out) {
-                if (err != null) {
-                    console.error(err);
-                    process.exit(1);
-                }
-                else {
-                    result+=out;
-                }
-            });
         }
+
+        //To select the type of template we are looking for
+        if (c.get('eSuperTypes').size() > 0) {
+            c.get('eSuperTypes').map(function (s) {
+                model.parentClass = s.get('name');
+            });
+            mapInherited.set(model.elementName, model);
+        } else {
+            map.set(model.elementName, model);
+        }
+
     });
 
+    //First without inheritance
+    map.forEach(function (elem) {
+        //We fill the template
+        DustJS.render("ClassWithoutInheritance", elem, function (err, out) {
+            if (err != null) {
+                console.error(err);
+                process.exit(1);
+            }
+            else {
+                result += out;
+            }
+        });
+    });
+
+    //Second with inheritance
+    mapInherited.forEach(function (elem) {
+        //We fill the template
+        DustJS.render("ClassInherited", elem, function (err, out) {
+            if (err != null) {
+                console.error(err);
+                process.exit(1);
+            }
+            else {
+                result += out;
+            }
+        });
+    });
+
+
     var elementsToExport = {};
-    elementsToExport.elements=[];
+    elementsToExport.elements = [];
     ePackage.get('eClassifiers').map(function (c) {
-        var element={};
-        element.name=c.get('name');
+        var element = {};
+        element.name = c.get('name');
         elementsToExport.elements.push(element);
     });
 
@@ -94,7 +122,7 @@ try {
         }
     });
     console.log(result);
-    eval(result);
+
 
 } catch (err) {
     console.log('*** Failed parsing metamodel');
